92f5be1940f9c2847a69aa0f10a8dd793bc9a7c2
Harsha Kumar <ahkharsha@gmail.com>
2024-10-31 21:10:36 +0530

    Minor fixes

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc1-testnet.emc.network";
-const NEXT_PUBLIC_PRIVATE_KEY = "";
+const NEXT_PUBLIC_PRIVATE_KEY = "fe22e08c4c07b0542221677f3af0d9450525cef20a15b2b65754262375fe8e03";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
